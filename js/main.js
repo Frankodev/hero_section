7139f270830367e3a8ec416a1e18d9122c6f9888
Francisco <xavicodev@gmail.com>
2022-01-05 16:29:26 -0600

    Se hacen cambios en el video y en los controles del video (play y muted) se hacen cambios en algunas clases en el whatsapp y se incorpora un nuevo archivo de .htaccess para ocultar la extension .html en la barra del navegador

--- js/main.js
+++ js/main.js
@@ -18,33 +18,3 @@ overlay.addEventListener("click", function () {
   showcase.classList.remove("active");
   overlay.classList.remove("active_overlay");
 });
-
-// función para pausar-continuar y poner-quitar audio al video de fondo
-// Variables
-const video = document.querySelector("#video");
-const pause = document.querySelector("#play");
-const volumen = document.querySelector("#muted");
-
-// función evento click para dar pause o play al video de fondo
-pause.addEventListener("click", () => {
-  if (video.paused) {
-    video.play();
-    play.name = "pause-outline";
-  } else {
-    video.pause();
-    play.name = "play-outline";
-  }
-});
-
-// función evento click para dar volumen o muted al video de fondo
-volumen.addEventListener("click", () => {
-  if (video.muted == true) {
-    video.volume = 1;
-    video.muted = false;
-    muted.name = "volume-high-outline";
-  } else {
-    video.volume = 0;
-    video.muted = true;
-    muted.name = "volume-mute-outline";
-  }
-});
